5b8625d4f6cc388fa71238d0c88b4f4411ed1b59
Gustavo Albertine <112908437+albtn9@users.noreply.github.com>
2025-11-10 12:44:34 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl:"https://nexdom.tec.br",
+    defaultCommandTimeout: 10000,
     setupNodeEvents(on, config) {
       
     },
